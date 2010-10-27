aea7d61c30b32b9f84be21203c493d59fb45818d
billsaysthis <blazar@gmail.com>
2010-10-27 14:12:53 -0700

    add version flag for cli

--- lib/ajaxboxlogin/cli.rb
+++ lib/ajaxboxlogin/cli.rb
@@ -1,11 +1,21 @@
 require 'thor' 
+require 'thor/actions' 
 require 'ajaxboxlogin/ajaxboxlogin'
+require 'ajaxboxlogin/version'
 
 module AjaxBoxLogin
   class CLI < Thor
+    include Thor::Actions
+    
     desc "gen view_path view_path javascript_lib", "generates view partial of template_engine in view_path, along with needed JavaScript based on javascript_lib framework and a stylesheet"
     def gen(template_engine, view_path, javascript_lib)
       AjaxBoxLogin::AjaxBoxLoginGen.gen_files(template_engine, view_path, javascript_lib)
-    end    
+    end
+    
+    desc "version", "prints out AjaxBoxLogin's version information"
+    def version
+      say "AjaxBoxLogin version #{AjaxBoxLogin::VERSION}"
+    end
+    map %w(-v --version) => :version
   end
 end
